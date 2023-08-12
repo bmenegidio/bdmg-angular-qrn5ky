8bbba76100dff98b51c2208c06946cd9deb65885
bmenegidio <brunomenegidio@hotmail.com>
2023-08-12 08:59:11 -0300

    feat: create models

---
+++ src/app/models/cep-model.ts
@@ -0,0 +1,12 @@
+export type CepModel = {
+  cep: string;
+  street: string;
+  complement: string;
+  neighborhood: string;
+  city: string;
+  uf: string;
+  ibgeCode: string;
+  gia: string;
+  ddd: string;
+  siafi: string;
+};
