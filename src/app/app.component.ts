4012522d265ca79b7f4c0475207477e1690de497
bmenegidio <brunomenegidio@hotmail.com>
2023-08-12 09:25:25 -0300

    feat: viaCep get & parse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { AppService } from './app.service';
+import { CepModel } from './models/cep-model';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+  cep = '30160907';
+  cepResult$: Observable<CepModel>;
+
+  constructor(private appService: AppService) {}
+
+  ngOnInit() {
+    this.cepResult$ = this.appService.getCep(this.cep);
+  }
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, take } from 'rxjs';
+
+import { CepModel } from './models/cep-model';
+import { ViaCepResponseModel } from './models/via-cep-response-model';
 
 @Injectable()
 export class AppService {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+
+  private parseViaCepResponse(viaCepResponse: ViaCepResponseModel): CepModel {
+    return {
+      cep: viaCepResponse.cep,
+      street: viaCepResponse.logradouro,
+      complement: viaCepResponse.complemento,
+      neighborhood: viaCepResponse.bairro,
+      city: viaCepResponse.localidade,
+      uf: viaCepResponse.uf,
+      ibgeCode: viaCepResponse.ibge,
+      gia: viaCepResponse.gia,
+      ddd: viaCepResponse.ddd,
+      siafi: viaCepResponse.siafi,
+    };
+  }
+
+  getCep(cep: string) {
+    return this.httpClient
+      .get<ViaCepResponseModel>(`https://viacep.com.br/ws/${cep}/json/`)
+      .pipe(
+        map((viaCepResponse) => this.parseViaCepResponse(viaCepResponse)),
+        take(1)
+      );
+  }
 }
